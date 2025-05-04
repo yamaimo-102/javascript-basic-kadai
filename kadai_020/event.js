6942d8b6706f92973fb1204ec1537345edb190b4
yamaimo-102 <myice.f01@gmail.com>
2025-05-04 17:36:43 +0900

    kadai_020を削除

--- kadai_020/event.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const Btn = document.getElementById('btn');
-    Btn.addEventListener('click', () => {
-        console.log('ボタンをクリックしました');
-    });
-});
